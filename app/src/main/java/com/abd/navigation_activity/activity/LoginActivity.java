c59b285a1c99db4e24fd50d9550514f8cbf09f04
Abdul Rahman <12050117089@students.uin-suska.ac.id>
2022-03-10 21:01:40 +0700

    Initial commit

---
+++ app/src/main/java/com/abd/navigation_activity/activity/LoginActivity.java
@@ -0,0 +1,52 @@
+package com.abd.navigation_activity.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.abd.navigation_activity.R;
+
+public class LoginActivity extends AppCompatActivity {
+    Button login;
+    EditText username,password;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.login_relative);
+
+        login = (Button) findViewById(R.id.btnLogin);
+        username = (EditText) findViewById(R.id.etusername);
+        password = (EditText) findViewById(R.id.etpw);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (password.getText().toString().toUpperCase().equals("TEST1"))
+                {
+                    login_sukses();
+                }else
+                {
+                    Toast.makeText(getApplicationContext(),"Salah password", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void login_sukses() {
+        String user_login = username.getText().toString();
+        Intent i = new Intent(this,MainActivity.class);
+        //Mengirimkan username nya ke tampilan utama
+        i.putExtra("USERNAME",user_login);
+
+        startActivity(i);
+
+    }
+}
